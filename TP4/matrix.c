6096678cefb390baaa4a559902f25a2cfe29bf4e
LaurenRolan <lauren.rolan@ecole.ensicaen.fr>
2018-11-08 10:19:33 +0100

    Update TP4

--- TP4/matrix.c
+++ TP4/matrix.c
@@ -7,25 +7,30 @@
 #include <unistd.h>
 #include <math.h>
 #include <sys/wait.h>
+#include <pthread.h>
 #include "matrix.h"
 
+int step = 0;
 
 /* Essayer de faire avec fork, et comparer avec le multithread */
 
-int alloc_matrix( double * matrix, int rows, int cols )
+double * alloc_matrix( int rows, int cols )
 {
-	matrix = malloc((rows * cols) * sizeof(double));
+	double * matrix;
+	matrix = (double*) malloc(rows * cols * sizeof(double));
 	fprintf(stderr, "Malloc successful\n");
-	return 0;
+	return matrix;
 }
 
-int random_matrix( double * matrix, int rows, int cols )
+int random_matrix( double * matrix, int rows, int cols, int seed )
 {
+	srand(seed);
 	double range = (MAX - MIN); 
     double div = RAND_MAX / range;
     for(int row = 0; row < rows; row++) {
     	for(int col = 0; col < cols; col++) {
-    		matrix[row * cols + col] = MIN + (rand() / div);
+    		double current_value = MIN + (rand() / div);
+    		matrix[row * cols + col] = current_value;
     	}
     }
     return 0;
@@ -34,13 +39,16 @@ int random_matrix( double * matrix, int rows, int cols )
 double * product_matrix( double * matrix1, int rows1, int cols1, double * matrix2, int rows2, int cols2 )
 {
 	double * result;
-	alloc_matrix(result, rows1, cols2);
+	result = alloc_matrix( rows1, cols2);
 	for( int row = 0; row < rows1; row++ ){
 		for(int col = 0; col < cols2; col++)
 		{	
-			int cell = 0;
-			for( int c = 0; c < cols2; c++ )
+			double cell = 0;
+			for( int c = 0; c < cols2; c++ ) {
+				fprintf(stderr, "Index = %d\tInit = %.3f\t", row * cols2 + col, cell); 
 				cell += matrix1[row * cols2 + col] * matrix2[row * cols2 + col];
+				fprintf(stderr, "Res = %.3f\tA = %.3f\tB = %.3f\n", cell, matrix1[row * cols2 + col], matrix2[row * cols2 + col]);
+			}
 			result[row * cols2 + col] = cell;
 		} 
 	}
@@ -49,7 +57,28 @@ double * product_matrix( double * matrix1, int rows1, int cols1, double * matrix
 
 double * product_matrix_thread(  double * matrix1, int rows1, int cols1, double * matrix2, int rows2, int cols2 )
 {
-	return NULL;
+	double * result;
+	result = alloc_matrix( rows1, cols2);
+	thread_struct current;
+	current.matrix1 = matrix1;
+	current.rows1 = rows1;
+	current.cols1 = cols1;
+	current.matrix2 = matrix2;
+	current.rows2 = rows2;
+	current.cols2 = cols2;
+	current.result = result;
+
+    pthread_t threads[MAX_THREAD]; 
+  
+    for (int i = 0; i < MAX_THREAD; i++) {
+        pthread_create(&threads[i], NULL, product_thread, (void*)(&current)); 
+    } 
+  
+    for (int i = 0; i < MAX_THREAD; i++) {
+        pthread_join(threads[i], NULL);  
+    }
+
+	return current.result;
 }
 
 double * product_matrix_fork( double * matrix1, int rows1, int cols1, double * matrix2, int rows2, int cols2  )
@@ -62,8 +91,25 @@ void print_matrix( double * matrix, int rows, int cols)
 	for( int row = 0; row < rows; row++ ){
 		printf("[ ");
 		for(int col = 0; col < cols; col++) {	
-			printf("%d ", matrix[row * cols + col]);	
+			printf("%3.3f ", matrix[row * cols + col]);	
 		}
 		printf("]\n");
 	}
 }
+
+void * product_thread(void* arg) 
+{
+	int sector = step++;
+	thread_struct * current = (thread_struct * ) arg;
+    for (int i = sector * current->rows1 / 4; i < (sector + 1) * current->rows1 / 4; i++) {
+    	for(int j = sector * current->cols2 / 4; j < (sector + 1) * current->cols2 / 4; j++) {
+    		for (int l = 0; l < current->rows1; l++) {
+	        	for (int k = 0; k < current->cols2; k++) {
+	        		fprintf(stderr, "Index = %d\tInit = %.3f\t", sector, current->result[i * current->cols2 + j]); 
+	                current->result[i * current->cols2 + j] += current->matrix1[i * current->cols1 + k] * current->matrix2[k * current->cols2 + l];
+	                fprintf(stderr, "Res = %.3f\tA = %.3f\tB = %.3f\n", current->result[i * current->cols2 + j], current->matrix1[i * current->cols1 + k], current->matrix2[k * current->cols2 + l]);
+	            } 
+	        }
+        }
+    }
+} 
--- TP4/matrix.h
+++ TP4/matrix.h
@@ -4,11 +4,23 @@
 
 #define MAX 10
 #define MIN -10
+#define MAX_THREAD 4
 
+typedef struct thread_struct {
+	double * matrix1;
+	int rows1;
+	int cols1;
+	double * matrix2;
+	int rows2;
+	int cols2;
+	double * result;
+	int sector;
+} thread_struct;
 
-int alloc_matrix( double * matrix, int rows, int cols );
-int random_matrix( double * matrix, int rows, int cols );
+double * alloc_matrix( int rows, int cols );
+int random_matrix( double * matrix, int rows, int cols, int seed );
 double * product_matrix( double * matrix1, int rows1, int cols1, double * matrix2, int rows2, int cols2 );
 double * product_matrix_thread( double * matrix1, int rows1, int cols1, double * matrix2, int rows2, int cols2 );
 double * product_matrix_fork( double * matrix1, int rows1, int cols1, double * matrix2, int rows2, int cols2 );
 void print_matrix( double * matrix, int rows, int cols );
+void* product_thread(void* arg);
